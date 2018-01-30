14bc015baa09c5d2a8fc6383f49ab0956f981e1e
chanel <caquino@csumb.edu>
2018-01-29 17:07:20 -0800

    reverting

--- TumblrFeed/TumblrFeed/PhotoCell.swift
+++ TumblrFeed/TumblrFeed/PhotoCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PhotoCell: UITableViewCell {
-    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- TumblrFeed/TumblrFeed/View Controllers/PhotosViewController.swift
+++ TumblrFeed/TumblrFeed/View Controllers/PhotosViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var postsTableView: UITableView! // outlet for posts
-    @IBOutlet weak var photoCell: PhotoCell!    // outlet for photos
+    // ---------------------------------
+    // outlets
+    // ---------------------------------
     
+    @IBOutlet weak var postsTableView: UITableView! // outlet for posts
+    @IBOutlet weak var photoCell: PhotoCell!        // outlet for photos
+    
+    // ---------------------------------
+    // properties
+    // ---------------------------------
     var posts: [[String: Any]] = [] // array of dictionaries from blog posts
     
+    
+    // ---------------------------------
+    // viewDidLoad
+    // ---------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,8 +35,11 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         postsTableView.dataSource = self
         
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
         session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        
         let task = session.dataTask(with: url) { (data, response, error) in
             if let error = error {
                 print(error.localizedDescription)
@@ -40,22 +55,32 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                 // TODO: Reload the table view
             }
         }
+        
         task.resume()
         // Do any additional setup after loading the view.
     }
-
+    
+    // ---------------------------------
+    // didReceiveMemoryWarning
+    // ---------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
-    // function to set number of rows
+    // ---------------------------------
+    // numberOfRows in Section
+    //      set number of rows
+    // ---------------------------------
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
     
-    // function to return the cell for each row
+    // ---------------------------------
+    // cellForRowAt
+    //      return the cell for each row
+    // ---------------------------------
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row] // pull out a single post from posts array
@@ -70,14 +95,21 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
             let urlString = originalSize["url"] as! String
             // creates a url
             let url = URL(string: urlString)
+            
+            // retrieve image from url
+            photoCell.photoImageView.af_setImage(withURL: url!)
         }
         let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
         cell.textLabel?.text = "This is row \(indexPath.row)"
-        
+        self.postsTableView.reloadData()
+
         return cell
     }
 
-    // dequeque the cell
+    // ---------------------------------
+    // cellForRowAtIndexPath
+    //      dequeque the cell
+    // ---------------------------------
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
         
@@ -85,6 +117,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         
         return cell
     }
+    
     /*
     // MARK: - Navigation
 
